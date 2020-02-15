4c6fd50d3411504fa5e944bd8f345c8c5ab7ab96
fabio-gz <fabiogomez.silva@gmail.com>
2020-02-15 22:18:21 +0000

    [Fix]typo

---
+++ models/base_model.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import uuid
+import datetime
+
+
+class BaseModel():
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+    def __str__(self):
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        self.__dict__['__class__'] = self.__class__.__name__
+        return (self.__dict__)
