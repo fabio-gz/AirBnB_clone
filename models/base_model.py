b8f7a5621a664c3312052678bee568ee848666ba
cristian0497 <cristian04.97@hotmail.com>
2020-02-15 19:39:27 -0500

    [ADD] Task 4, create an instance of object dictionary repr

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,19 @@ import uuid
 import datetime
 
 
-class BaseModel():
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if (kwargs):
+            for key, value in kwargs.items():
+                if (key == 'created_at' or key == 'updated_at'):
+                    setattr(self, key, datetime.datetime.fromisoformat(value))
+                else:
+                    if (key != '__class__'):
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
