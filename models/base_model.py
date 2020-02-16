218d1a3adea645439fdaacb46c6bacd69cdd646e
fabio-gz <fabiogomez.silva@gmail.com>
2020-02-16 16:07:08 +0000

    [update]time format

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 import uuid
 import datetime
+ftime = '%Y-%m-%dT%H:%M:%S.%f'
 
-
-class BaseModel():
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if (kwargs):
+            for key, value in kwargs.items():
+                if (key == 'created_at' or key == 'updated_at'):
+                    setattr(self, key, datetime.datetime.strptime(value, ftime))
+                else:
+                    if (key != '__class__'):
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                      self.__dict__))
 
     def save(self):
         self.updated_at = datetime.datetime.now()
