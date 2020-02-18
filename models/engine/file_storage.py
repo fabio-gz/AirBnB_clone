9ac4ca029c3e1ff803344d8fc62dc722fcc2c8cc
cristian0497 <cristian04.97@hotmail.com>
2020-02-17 19:49:59 -0500

    [ADD] Tasj 8, 9 and 10. clases with attributes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,8 +21,15 @@ class FileStorage:
 
     def reload(self):
         from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
-        dic = {'BaseModel' : BaseModel}
+        dic = {'BaseModel' : BaseModel, 'User': User, 'Place': Place,
+        'State': State, 'City': City, 'Amenity': Amenity, 'Review': Review}
         try:
             with open(FileStorage.__file_path, mode="r") as f:
                 obj = json.load(f)
---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
