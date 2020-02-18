a24b190e582d206d930908c15ddcbe69b2d505aa
cristian0497 <cristian04.97@hotmail.com>
2020-02-17 21:56:25 -0500

    [FIX] correction. quit self. of classes

--- models/user.py
+++ models/user.py
@@ -3,7 +3,7 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    self.email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
