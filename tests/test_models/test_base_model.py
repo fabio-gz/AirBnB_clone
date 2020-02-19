45196106297b1adefa0df7aa55121cf8c1f5d852
fabio-gz <fabiogomez.silva@gmail.com>
2020-02-19 15:18:52 +0000

    [Update] more cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,22 +14,46 @@ class test_BaseModel(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        """run methof prior test"""
+        """run methof prior test
+        """
         self.my_model = BaseModel()
         self.my_model.name = 'Skywalker'
         self.my_model.my_number = 70
 
     def Testpep8(self):
-        """test for pep8 styleguide"""
+        """test for pep8 styleguide
+        """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/base_model.py'])
         self.assertEqual(result.total_errors, 0,
                          'Found code style errors (and warnings).')
 
     def test_data(self):
+        """data asignment
+        """
         self.assertIsInstance(self.my_model.id, str)
         self.assertEqual(self.my_model.name, 'Skywalker')
         self.assertEqual(self.my_model.my_number, 70)
         self.assertIsInstance(self.my_model, BaseModel)
         self.assertIsInstance(self.my_model.created_at, datetime.datetime)
         self.assertIsInstance(self.my_model.updated_at, datetime.datetime)
+
+    def test_save(self):
+        """test save method to update instance attribute
+        """
+        self.my_model = BaseModel()
+        self.my_model.save()
+
+        self.assertIsInstance(self.my_model.created_at, datetime.datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime.datetime)
+        self.assertNotEqual(self.my_model.created_at, self.my_model.updated_at)
+
+    def test_to_dict(self):
+        """test toc check dictionary values of the instance
+        """
+        self.my_model = BaseModel()
+        d = self.my_model.to_dict()
+
+        self.assertEqual(d['__class__'], 'BaseModel')
+        self.assertEqual(d['created_at'], self.my_model.created_at.isoformat())
+        self.assertEqual(d['updated_at'], self.my_model.updated_at.isoformat())
